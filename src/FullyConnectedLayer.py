1c372a95ae3688cf5b5e3142f632845b22d8aae3
kerem-arikan <kerembio@gmail.com>
2020-07-16 13:30:44 +0300

    Added FC forward prop

--- src/FullyConnectedLayer.py
+++ src/FullyConnectedLayer.py
@@ -11,17 +11,30 @@ class FullyConnectedLayer:
 
 
     def forward_prop(self, input_list):
-        output = np.zeros((self.perc_count, self.weight_count))
-        # todo: write a forward propogation algorithm here
+        output_list = np.zeros((self.perc_count, self.weight_count))
+        (prev_layer_perc_count, prev_layer_weight_count) = input_list.shape
+
+        assert prev_layer_weight_count == self.perc_count
+
+        for perc_idx in range(0, self.perc_count):
+            total_input = []
+            for prev_layer_perc_idx in range(0, prev_layer_perc_count):
+                total_input.append(input_list[prev_layer_perc_idx][perc_idx])
+            total_input = np.array(total_input)
+            output_list[perc_idx] = self.neurons[perc_idx].calc_output(total_input)
+    
+
+
+                
 
 class Perceptron:
     def __init__(self, init_output, weight_count):
         self.output = init_output
         self.weight_count = weight_count
-        self.weight = []
+        self.weights = []
         for w_idx in range(0, self.weight_count):
-            self.weight.append(1)
-        self.weight = np.array(self.weight)
+            self.weights.append(0.0)
+        self.weights = np.array(self.weight)
     
     def ReLU(self, input):
         return max([0, input])
@@ -29,4 +42,4 @@ class Perceptron:
     def calc_output(self, input_list):
         total = np.sum(input_list)
         self.output = self.ReLU(total)
-        return self.output * self.weight
+        return self.output * self.weights
